05a1c3e00044aa1d196ab0ff923738ce5bbb4e40
justin lee <yuminjustin@163.com>
2017-11-14 18:19:43 +0800

    Update meta.js

--- meta.js
+++ meta.js
@@ -3,17 +3,17 @@ module.exports = {
     "name": {
       "type": "string",
       "required": true,
-      "message": "项目名称"
+      "message": "项目名称/ project name"
     },
     "description": {
       "type": "string",
       "required": false,
-      "message": "项目描述",
+      "message": "项目描述/ description",
       "default": "A babckbone project"
     },
     "author": {
       "type": "string",
-      "message": "作者"
+      "message": "作者/ author"
     }
   },
   "completeMessage": "开始你的项目:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\n"
