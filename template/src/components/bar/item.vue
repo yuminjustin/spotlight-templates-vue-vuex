200641f6c7fe8548691268efe40e24cf8cdfade0
justin lee <yuminjustin@163.com>
2017-12-11 15:09:50 +0800

    Update item.vue

--- template/src/components/bar/item.vue
+++ template/src/components/bar/item.vue
@@ -2,14 +2,14 @@
     <div>
         <template v-if="item.group">
             <el-submenu :index="String(idx)">
-                <template slot="title">{{item.name}}</template>
+                <template slot="title">\{{item.name}}</template>
                 <template v-for="(son,idx2) in item.group">
                     <Item :item="son" :key="idx+'-'+idx2" :idx="idx+'-'+idx2" @openPage="openPage" />
                 </template>
             </el-submenu>
         </template>
         <template v-else>
-            <el-menu-item :index="String(idx)" @click="openPage(item)">{{item.name}}</el-menu-item>
+            <el-menu-item :index="String(idx)" @click="openPage(item)">\{{item.name}}</el-menu-item>
         </template>
     </div>
 </template>
