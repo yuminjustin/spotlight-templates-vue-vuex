c41068b5cc45d2a8a3f7f9ca978bd10ab86176d3
justin lee <yuminjustin@163.com>
2018-12-27 14:10:49 +0800

    Update App.vue

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <Top />
-    <el-row class="c main_body oh">
+    <el-row :class="style.main_body">
       <el-col :span="3">
         <Bar :style="{'height':h+'px'}" />
       </el-col>
-      <el-col :span="21" :style="{'height':h+'px'}" class="oa main_content">
+      <el-col :span="21" :style="{'height':h+'px'}" :class="style.main_content">
         <router-view></router-view>
       </el-col>
     </el-row>
@@ -15,11 +15,13 @@
 <script>
   import Top from 'C/head'
   import Bar from 'C/bar'
+  import style from "B/assets/css/page.css"
 
   export default {
     name: 'app',
     data() {
       return {
+       style,
         h: window.innerHeight - 100
       }
     },
